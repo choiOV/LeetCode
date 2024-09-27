aa765111467f7c35a49b9d2fdc8e59143f56b030
choiOV <121269364+choiOV@users.noreply.github.com>
2024-09-28 00:22:29 +0900

    Time: 69 ms (26.72%), Space: 49.8 MB (79.73%) - LeetHub

--- 2629-function-composition/2629-function-composition.js
+++ 2629-function-composition/2629-function-composition.js
@@ -1,16 +1,12 @@
 const compose = (functions) => {
-  return function (x) {
-    if (functions.length === 0) return x;
-    let result;
+    return function (x) {
+        if (functions.length === 0) return x;
+        let result = x;
 
-    for (let i = functions.length - 1; i >= 0; i--) {
-      if (!result) {
-        result = functions[i](x);
-      } else {
-        result = functions[i](result);
-      }
-    }
+        for (let i = functions.length - 1; i >= 0; i--) {
+            result = functions[i](result);
+        }
 
-    return result;
-  };
+        return result;
+    };
 };
