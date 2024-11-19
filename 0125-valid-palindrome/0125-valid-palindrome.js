84f2d566a30aa27a781d7558111f0a61248cb4b3
choiOV <121269364+choiOV@users.noreply.github.com>
2024-11-19 23:16:31 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.js
+++ 0125-valid-palindrome/0125-valid-palindrome.js
@@ -2,57 +2,37 @@
  * @param {string} s
  * @return {boolean}
  */
-var isPalindrome = function(s) {
-  let res = s.replaceAll(" ", "");
-  console.log(res)
-  
-  const a = res.toLowerCase().split("");
-  console.log(a)
-  
-  const b = a.filter((str) => {
-    if (
-      str === "a" ||
-      str === "b" ||
-      str === "c" ||
-      str === "d" ||
-      str === "e" ||
-      str === "f" ||
-      str === "g" ||
-      str === "h" ||
-      str === "i" ||
-      str === "j" ||
-      str === "k" ||
-      str === "l" ||
-      str === "m" ||
-      str === "n" ||
-      str === "o" ||
-      str === "p" ||
-      str === "q" ||
-      str === "r" ||
-      str === "s" ||
-      str === "t" ||
-      str === "u" ||
-      str === "v" ||
-      str === "w" ||
-      str === "x" ||
-      str === "y" ||
-      str === "z" ||
-      isNaN(str) === false
-    ) {
-      return str;
-    }
-  });
-  
-  let index = b.length - 1;
-  let result = true;
-  const c = [...b].forEach((e, i, a) => {
-    if (e !== a[index]) {
-      result = false;
-      return;
-    }
+var isPalindrome = function (s) {
+    if (s.length === 0) return true;
 
-    index -= 1;
-  });
+    let start = 0;
+    let last = s.length - 1;
 
-  return result;
+    const isLetterOrDigit = (c) => {
+        const code = c.charCodeAt(0);
+        return (
+            (code >= 48 && code <= 57) ||
+            (code >= 65 && code <= 90) ||
+            (code >= 97 && code <= 122)
+        );
+    };
+
+    while (start <= last) {
+        const currFirst = s[start];
+        const currLast = s[last];
+
+        if (!isLetterOrDigit(currFirst)) {
+            start++;
+        } else if (!isLetterOrDigit(currLast)) {
+            last--;
+        } else {
+            if (currFirst.toLowerCase() !== currLast.toLowerCase()) {
+                return false;
+            }
+
+            start++;
+            last--;
+        }
+        return true;
+    }
 };
