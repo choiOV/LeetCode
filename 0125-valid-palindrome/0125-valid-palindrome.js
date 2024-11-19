3b8dc2ac95542a9f86f1ee654dbd3a8da81dcf45
choiOV <121269364+choiOV@users.noreply.github.com>
2024-11-19 23:20:16 +0900

    Time: 4 ms (72.26%), Space: 52.5 MB (57.33%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.js
+++ 0125-valid-palindrome/0125-valid-palindrome.js
@@ -3,24 +3,14 @@
  * @return {boolean}
  */
 var isPalindrome = function (s) {
-    if (s.length === 0) return true;
-
+    if (s.length === 0) {
+        return true;
+    }
     let start = 0;
     let last = s.length - 1;
-
-    const isLetterOrDigit = (c) => {
-        const code = c.charCodeAt(0);
-        return (
-            (code >= 48 && code <= 57) ||
-            (code >= 65 && code <= 90) ||
-            (code >= 97 && code <= 122)
-        );
-    };
-
     while (start <= last) {
-        const currFirst = s[start];
-        const currLast = s[last];
-
+        let currFirst = s[start];
+        let currLast = s[last];
         if (!isLetterOrDigit(currFirst)) {
             start++;
         } else if (!isLetterOrDigit(currLast)) {
@@ -29,10 +19,18 @@ var isPalindrome = function (s) {
             if (currFirst.toLowerCase() !== currLast.toLowerCase()) {
                 return false;
             }
-
             start++;
             last--;
         }
-        return true;
     }
+    return true;
+}
+
+function isLetterOrDigit(c) {
+    const code = c.charCodeAt(0);
+    return (
+        (code >= 48 && code <= 57) ||    // 숫자 (0-9)
+        (code >= 65 && code <= 90) ||    // 대문자 (A-Z)
+        (code >= 97 && code <= 122)      // 소문자 (a-z)
+    );
 };
