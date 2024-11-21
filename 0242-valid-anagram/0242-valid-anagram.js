384ab26ebffec2d0b69d2000b3fc517339e50bc1
choiOV <121269364+choiOV@users.noreply.github.com>
2024-11-21 21:22:55 +0900

    Time: 11 ms (84.33%), Space: 50.9 MB (86.54%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.js
+++ 0242-valid-anagram/0242-valid-anagram.js
@@ -1,20 +1,14 @@
-/**
- * @param {string} s
- * @param {string} t
- * @return {boolean}
- */
 var isAnagram = function (s, t) {
     if (s.length !== t.length) return false;
-
     const charCount = {};
 
-    for (let i = 0; i < s.length - 1; i++) {
+    for (let i = 0; i < s.length; i++) {
         const char = s[i];
 
         charCount[char] = (charCount[char] || 0) + 1;
     }
 
-    for (let i = 0; i < t.length - 1; i++) {
+    for (let i = 0; i < t.length; i++) {
         const char = t[i];
 
         if (!charCount[char]) return false;
