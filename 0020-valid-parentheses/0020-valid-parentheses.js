4c87fade8f7b6328db25e874b139a036c02b8819
choiOV <121269364+choiOV@users.noreply.github.com>
2024-11-23 21:16:22 +0900

    Time: 6 ms (26.12%), Space: 52.3 MB (11.08%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.js
+++ 0020-valid-parentheses/0020-valid-parentheses.js
@@ -3,25 +3,23 @@
  * @return {boolean}
  */
 var isValid = function (s) {
-    if (s.length === 1) return false;
+    if (s.length % 2 !== 0) return false;
 
-    const arr = [...s];
     const stack = [];
 
-    for (const falind of arr) {
-        if (falind === "(" || falind === "[" || falind === "{") {
-            stack.push(falind);
+    for (const bracket of s) {
+        if (bracket === "(" || bracket === "[" || bracket === "{") {
+            stack.push(bracket);
         } else if (
-            stack[stack.length - 1] + falind === "()" ||
-            stack[stack.length - 1] + falind === "[]" ||
-            stack[stack.length - 1] + falind === "{}"
+            stack[stack.length - 1] + bracket === "()" ||
+            stack[stack.length - 1] + bracket === "[]" ||
+            stack[stack.length - 1] + bracket === "{}"
         ) {
             stack.pop();
         } else {
             return false;
         }
     }
-    if (stack.length !== 0) return false;
 
-    return true;
+    return !stack.length;
 };
