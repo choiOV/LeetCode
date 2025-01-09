1c673556752169f708d9dcc45427b4cb267ead9c
choiOV <121269364+choiOV@users.noreply.github.com>
2025-01-10 00:28:11 +0900

    Time: 73 ms (20.13%), Space: 53 MB (65.42%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.js
+++ 0141-linked-list-cycle/0141-linked-list-cycle.js
@@ -18,10 +18,10 @@ var hasCycle = function (head) {
 
     while (fast && fast.next) {
         slow = slow.next;
-        fast = fast.next.next;
+        fast = fast.next.next
 
-        if (slow === fast) return true;
+        if (slow === fast) return true
     }
 
-    return false;
+    return false
 };
