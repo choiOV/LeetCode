2c36baac93e1ec5a9d488d351e3dd1eebe2d0389
choiOV <121269364+choiOV@users.noreply.github.com>
2025-01-18 00:24:46 +0900

    Time: 30 ms (24.47%), Space: 54.6 MB (18.98%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.js
+++ 0242-valid-anagram/0242-valid-anagram.js
@@ -1,16 +1,8 @@
-var isAnagram = function (s, t) {
-    if (s.length !== t.length) return false;
-
-    const charCount = {};
-
-    for (const char of s) {
-        charCount[char] = (charCount[char] || 0) + 1;
-    }
-
-    for (const char of t) {
-        if (!charCount[char]) return false;
-        charCount[char]--;
-    }
-
-    return true;
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function(s, t) {
+    return [...s].sort().join() === [...t].sort().join();
 };
