ddb1532ef25ec6d3bd6b5e18e947492b3bbb91d8
choiOV <121269364+choiOV@users.noreply.github.com>
2025-01-18 00:30:41 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.js
+++ 0242-valid-anagram/0242-valid-anagram.js
@@ -3,6 +3,21 @@
  * @param {string} t
  * @return {boolean}
  */
-var isAnagram = function(s, t) {
-    return [...s].sort().join() === [...t].sort().join();
+var isAnagram = function (s, t) {
+  const hash = {};
+  
+  for (const str of s) {
+    hash[str] = (hash[str] || 0) + 1;
+  }
+
+  for (const str of t) {
+    if (hash[str] === undefined) return false;
+    hash[str] -= 1;
+  }
+
+  for (const count in hash) {
+    if (count > 0) return false;
+  }
+
+  return true;
 };
