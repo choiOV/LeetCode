a71b6d6a315dfec9c7f3da19ff065486d716d001
choiOV <121269364+choiOV@users.noreply.github.com>
2025-01-29 23:40:44 +0900

    Time: 79 ms (19.31%), Space: 59.5 MB (26.75%) - LeetHub

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.js
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.js
@@ -13,15 +13,11 @@
  * @return {TreeNode}
  */
 var lowestCommonAncestor = function (root, p, q) {
-    if (root === null) return
-
-    if (root.val < p.val && root.val < q.val) {
-        return lowestCommonAncestor(root.right, p, q);
-    }
-
-    else if (root.val > p.val && root.val > q.val) {
+    if (root.val > p.val && root.val > q.val) {
         return lowestCommonAncestor(root.left, p, q);
+    } else if (root.val < p.val && root.val < q.val) {
+        return lowestCommonAncestor(root.right, p, q);
     } else {
-        return root;
+        return root
     }
 };
