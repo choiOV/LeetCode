2981ce849aad154da33b9ceeaee9665fd57ec089
choiOV <121269364+choiOV@users.noreply.github.com>
2025-06-20 07:30:29 +0900

    Time: 1312 ms (5.16%), Space: 67 MB (20.19%) - LeetHub

--- 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.js
+++ 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.js
@@ -1,22 +1,17 @@
 var removeDuplicates = function (s, k) {
   const stack = [];
 
-  for (let char of s) {
-    if (stack.length && stack[stack.length - 1][0] === char) {
-      stack[stack.length - 1][1] += 1;
-      if (stack[stack.length - 1][1] === k) {
-        stack.pop();
+  for (const str of s) {
+    stack.push(str);
+
+    if (stack.length >= k) {
+      const checkSameCharArr = stack.slice(-k);
+
+      if (checkSameCharArr.every((e, i, a) => a[0] === e)) {
+        stack.splice(-k);
       }
-    } else {
-      stack.push([char, 1]);
     }
   }
 
-  let res = '';
-
-  for (let [char, count] of stack) {
-    res += char.repeat(count);
-  }
-
-  return res;
+  return stack.join("");
 };
