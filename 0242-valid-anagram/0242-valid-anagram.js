b8c5d570a023a90d75e2c75526e6f529809bd502
choiOV <121269364+choiOV@users.noreply.github.com>
2025-06-26 17:00:58 +0900

    Time: 23 ms (32.91%), Space: 65.9 MB (5.09%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.js
+++ 0242-valid-anagram/0242-valid-anagram.js
@@ -1,32 +1,22 @@
 var isAnagram = function (s, t) {
   if (s.length !== t.length) return false;
-  const hash = {};
 
-  for (const char of s) {
-    const lower = char.toLowerCase();
-    if (lower.charCodeAt() < 97 || lower.charCodeAt() > 122) continue;
+  const normalize = (str) =>
+    [...str.toLowerCase()].filter((c) => c >= "a" && c <= "z");
 
-    if (!hash[lower]) {
-      hash[lower] = 1;
-      continue;
-    }
+  const sArr = normalize(s);
+  const tArr = normalize(t);
+  const counter = {};
 
-    if (hash[lower]) {
-      hash[lower] += 1;
-    }
+  for (const c of sArr) {
+    counter[c] = (counter[c] || 0) + 1;
   }
 
-  for (const char of t) {
-    const lower = char.toLowerCase();
+  for (const c of tArr) {
+    if (!counter[c]) return false;
 
-    if (hash[lower] < 0) return false;
-
-    if (hash[lower]) {
-      hash[lower] -= 1;
-    }
+    counter[c] -= 1;
   }
 
-  const hashArray = Object.values(hash);
-
-  return hashArray.every((count) => count <= 0);
+  return true;
 };
