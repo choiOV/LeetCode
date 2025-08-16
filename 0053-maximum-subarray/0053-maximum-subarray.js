3b707c87659bf4206a364e58a17b353b199d596a
choiOV <121269364+choiOV@users.noreply.github.com>
2025-08-16 23:28:54 +0900

    Time: 8 ms (6.3%), Space: 66.1 MB (5.49%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.js
+++ 0053-maximum-subarray/0053-maximum-subarray.js
@@ -3,13 +3,13 @@
  * @return {number}
  */
 var maxSubArray = function (nums) {
-    let currentSum = nums[0];
-    let maxSum = nums[0];
+  let maxSum = nums[0];
+  let currentSum = 0;
 
-    for (let i = 1; i < nums.length; i++) {
-        currentSum = Math.max(nums[i], currentSum + nums[i]);
-        maxSum = Math.max(currentSum, maxSum);
-    }
+  for (const num of nums) {
+    currentSum = Math.max(num, currentSum + num);
+    maxSum = Math.max(currentSum, maxSum);
+  }
 
-    return maxSum;
+  return maxSum;
 };
