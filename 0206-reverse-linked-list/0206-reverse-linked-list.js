fa3cecb3d29da2f499aad4859ef6fb55095631bc
choiOV <121269364+choiOV@users.noreply.github.com>
2025-09-01 23:22:36 +0900

    Time: 0 ms (100%), Space: 57.9 MB (27.18%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.js
+++ 0206-reverse-linked-list/0206-reverse-linked-list.js
@@ -1,9 +1,21 @@
+/**
+ * Definition for singly-linked list.
+  단일 연결 리스트에 대한 정의입니다.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
 var reverseList = function (head) {
   let prev = null;
   let curr = head;
 
   while (curr !== null) {
-    let next = curr.next;
+    const next = curr.next;
     curr.next = prev;
     prev = curr;
     curr = next;
