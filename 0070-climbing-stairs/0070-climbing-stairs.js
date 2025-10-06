2025c82e22bcc09892f511cd111aed09f04b4f94
choiOV <121269364+choiOV@users.noreply.github.com>
2025-10-07 08:55:01 +0900

    Time: 0 ms (100%), Space: 53.6 MB (53.97%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var climbStairs = function (n) {
+  if (n <= 2) return n;
+
+  const fibo = [1, 1, 2];
+
+  for (let i = 3; i <= n; i++) {
+    fibo[i] = fibo[i - 1] + fibo[i - 2];
+  }
+
+  return fibo[n];
+};
