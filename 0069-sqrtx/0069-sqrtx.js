1860463f3a2dd4b99408bd9b4153acc1d7a7bb39
choiOV <121269364+choiOV@users.noreply.github.com>
2025-11-13 23:21:50 +0900

    Time: 1 ms (67.22%), Space: 57.4 MB (7.42%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number} x
+ * @return {number}
+ */
+var mySqrt = function (x) {
+  if (x < 2) return x;
+
+  let left = 1;
+  let right = Math.floor(x / 2);
+  let result = 0;
+
+  while (left <= right) {
+    let mid = Math.floor((left + right) / 2);
+    let sq = mid * mid;
+
+    if (sq === x) return mid;
+    if (sq < x) {
+      result = mid;
+      left = mid + 1;
+    } else {
+      right = mid - 1;
+    }
+  }
+
+  return result;
+};
