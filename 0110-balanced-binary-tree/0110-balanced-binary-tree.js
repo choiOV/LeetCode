7bfadbbeb97ef7afb080591f42087a3ef8b7c7da
choiOV <121269364+choiOV@users.noreply.github.com>
2025-11-19 23:41:16 +0900

    Time: 1 ms (85.84%), Space: 61.1 MB (17.29%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.js
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.js
@@ -11,18 +11,22 @@
  * @return {boolean}
  */
 var isBalanced = function (root) {
-    const dfs = (node) => {
-        if (!node) return 0;
+  function dfs(node) {
+    if (node === null) return 0;
 
-        let leftHeight = dfs(node.left);
-        let rightHeight = dfs(node.right);
+    const leftHeight = dfs(node.left);
+    const rightHeight = dfs(node.right);
 
-        if (leftHeight === -1 || rightHeight === -1 || Math.abs(leftHeight - rightHeight) > 1) {
-            return -1;
-        }
-
-        return Math.max(leftHeight, rightHeight) + 1;
+    if (leftHeight === -1 || rightHeight === -1) {
+      return -1;
     }
 
-    return dfs(root) !== -1;
+    if (Math.abs(leftHeight - rightHeight) > 1) {
+      return -1;
+    }
+
+    return Math.max(leftHeight, rightHeight) + 1;
+  }
+
+  return dfs(root) !== -1;
 };
